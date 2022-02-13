621a835b27c380f133c9508ad98655a15e6de8cf
timwolf8503 <96329797+timwolf8503@users.noreply.github.com>
2022-02-13 08:35:59 +0800

    Update 練習程式

---
+++ Unity_2D_Homework/Assets/script/Learn.cs
@@ -0,0 +1,16 @@
+
+using System;
+using UnityEngine;
+
+public class Learn : MonoBehaviour
+{
+    #region
+    public int hight;
+    public float weight = 50;
+
+    public Color colorA;
+    public Color yellow = Color.yellow;
+
+
+#endregion
+}
