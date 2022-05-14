2f6dc0791ca48e89007dbf0a82c14846da30b16e
timwolf8503 <96329797+timwolf8503@users.noreply.github.com>
2022-05-14 20:51:36 +0800

    NOn

---
+++ Unity_2D_Homework/Assets/script/Learnstare.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Learnstare : MonoBehaviour
+{
+    public int a = 10;
+    public int b = 100;
+    public int c = 5;
+    public int e = 2;
+    public  Color  colora=Color.green ;
+  
+
+
+
+
+    private void Start()
+    {
+        
+
+    }
+
+
+
+
+
+
+}
